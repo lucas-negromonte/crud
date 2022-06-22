c1febea3a32294d3d53ce2233970a5bb67a631ab
lucas-negromonte <lcs.mj.zl@gmail.com>
2022-06-21 23:56:37 -0300

    Add Config

---
+++ source/Boot/Config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Main Setup
+ */
+define("CONF_URL_BASE", "https://crud-01.000webhostapp.com");
+define("CONF_URL_TEST", "https://localhost/crud");
+define("CONF_SITE_NAME", "Sistema CRUD em PHP Orientado a Objetos e MVC.");
+define("CONF_MAIL", "lcs.mj.zl@gmail.com");
+define("CONF_ADM_EMAIL", "lcs.mj.zl@gmail.com");
+
+/**
+ * View Setup
+ */
+define("CONF_VIEW_PATH", __DIR__ . "/../../themes");
+define("CONF_VIEW_EXT", "php");
+define("CONF_VIEW_THEME", "template-one");
+
+/** 
+ * Database Setup 
+ */
+define("CONF_DB_HOST",'localhost');
+define("CONF_DB_USER", "root");
+define("CONF_DB_PASS", '');
+define("CONF_DB_NAME", "crud"); 
+
+define("CONF_TIMEZONE", "-03:00");
