6109fa3723790843fb1621c187ec661a663f9201
lucas-negromonte <lcs.mj.zl@gmail.com>
2022-06-21 23:41:52 -0300

    update database

--- database.sql
+++ database.sql
@@ -12,11 +12,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Copiando estrutura do banco de dados para crud
-CREATE DATABASE IF NOT EXISTS `crud` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `crud`;
-
 -- Copiando estrutura para tabela crud.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -24,12 +19,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(50) NOT NULL COMMENT 'Email do usuario',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COMMENT='Tabela de usuarios';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='Tabela de usuarios';
 
 -- Copiando dados para a tabela crud.users: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `name`, `email`) VALUES
-	(1, 'Lucas Almeida Negromonte', 'lcs.mj.zl@gmail.com');
+	(1, 'Lucas Negromonte', 'lcs.mj.zl@gmail.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -3,7 +3,7 @@
 /**
  * Main Setup
  */
-define("CONF_URL_BASE", "https://github.com/lucas-negromonte");
+define("CONF_URL_BASE", "https://crud-01.000webhostapp.com");
 define("CONF_URL_TEST", "https://localhost/crud");
 define("CONF_SITE_NAME", "Sistema CRUD em PHP Orientado a Objetos e MVC.");
 define("CONF_MAIL", "lcs.mj.zl@gmail.com");
@@ -19,8 +19,16 @@ define("CONF_VIEW_THEME", "template-one");
 /** 
  * Database Setup 
  */
+// define("CONF_DB_HOST",'localhost');
+// define("CONF_DB_USER", "root");
+// define("CONF_DB_PASS", '');
+// define("CONF_DB_NAME", "crud"); 
+
 define("CONF_DB_HOST",'localhost');
-define("CONF_DB_USER", "root");
-define("CONF_DB_PASS", '');
-define("CONF_DB_NAME", "crud"); 
+define("CONF_DB_USER", "id19148818_db_crud");
+define("CONF_DB_PASS", '+kCdArWlK-Cg#vC5');
+define("CONF_DB_NAME", "id19148818_crud"); 
+
+// 
+
 define("CONF_TIMEZONE", "-03:00");
